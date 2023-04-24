976ef1a249dccdec7aec6fb057c8f01b87169d57
adamwilkolazki <100996396+adamwilkolazki@users.noreply.github.com>
2023-04-24 19:39:01 +0200

    Delete library directory

--- library/src/pl/javastart/library/exception/DataExportEception.java
+++
@@ -1,7 +0,0 @@
-package pl.javastart.library.exception;
-
-public class DataExportEception extends RuntimeException{
-    public DataExportEception(String message) {
-        super(message);
-    }
-}
--- library/src/pl/javastart/library/exception/PublicationAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package pl.javastart.library.exception;
-
-public class PublicationAlreadyExistException extends RuntimeException{
-    public PublicationAlreadyExistException(String message) {
-        super(message);
-    }
-}
